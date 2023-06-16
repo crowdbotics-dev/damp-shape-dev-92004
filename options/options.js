1517008278e7e126501722c514b36d287dee0db4
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 06:08:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-shape-dev-92004",
+  url: "https://damp-shape-dev-92004.botics.co",
+  api: "https://damp-shape-dev-92004.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
